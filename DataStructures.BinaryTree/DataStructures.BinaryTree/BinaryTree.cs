ec8ca0d1ed26230a68d5f03244a462f7d5b9d1fb
chnandakumarreddy <chnandakumarreddy@gmail.com>
2016-03-17 00:19:25 -0700

    Included BinaryTree Implementation -Insert() and Search()

---
+++ DataStructures.BinaryTree/DataStructures.BinaryTree/BinaryTree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinaryTree
+{
+    public class BinaryTree<T>
+    {
+        public class Node
+        {
+            public T data { get; set; }
+            public Node right { get; set; }
+            public Node left { get; set; }
+
+            public Node(T nodeData)
+            {
+                data = nodeData;
+                right = null;
+                left = null;
+            }
+        }
+
+        public BinaryTree()
+        {
+            headNode = null;
+            Count = 0;
+        }
+
+        public int Count { get; private set; }
+        private Node headNode { get; set; }
+
+        public Node Insert(T data)
+        {
+            Node newNode = new Node(data);
+            if (headNode == null)
+            {
+                headNode = newNode;
+                newNode.left = newNode.right = null;
+                Count++;
+                return headNode;
+            }
+            else
+            {
+                return InsertNewNode(headNode, data);
+            }
+            
+        }
+
+        private Node InsertNewNode(Node rootNode, T data)
+        {
+            if (rootNode == null)
+            {
+                rootNode = new Node(data);
+                rootNode.left = rootNode.right = null;
+                Count++;
+            }
+            else
+            {
+                if (Comparer<T>.Default.Compare(data, rootNode.data) < 0)
+                {
+                    rootNode.left = InsertNewNode(rootNode.left, data);
+                }
+                else
+                {
+                    rootNode.right = InsertNewNode(rootNode.right, data);
+                }
+            }
+            return rootNode;
+        }
+
+        public Node SearchBinaryTree(T data)
+        {
+            return SearchBinaryTreeRecursively(headNode, data);
+        }
+
+        private Node SearchBinaryTreeRecursively(Node parentNode, T data)
+        {
+            if (parentNode == null)
+            {
+                return null;
+            }
+            if (parentNode.data.Equals(data))
+            {
+                return parentNode;
+            }
+            else
+            {
+                if (Comparer<T>.Default.Compare(data, parentNode.data) < 0)
+                {
+                    return SearchBinaryTreeRecursively(parentNode.left, data);
+                }
+                else
+                {
+                    return SearchBinaryTreeRecursively(parentNode.right, data);
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures.BinaryTree/DataStructures.BinaryTreeTest/BinaryTreeUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.BinaryTree
+{
+    [TestClass]
+    public class BinaryTreeUnitTest
+    {
+        [TestMethod]
+        public void CanInsertIntoBinaryTree()
+        {
+            var binaryTree = new BinaryTree<int>();
+            binaryTree.Insert(10);
+            binaryTree.Insert(50);
+            binaryTree.Insert(30);
+            binaryTree.Insert(7);
+            binaryTree.Insert(100);
+            Assert.IsTrue(binaryTree.Count == 5);
+        }
+
+        [TestMethod]
+        public void CanSearchBinaryTree()
+        {
+            var binaryTree = new BinaryTree<int>();
+            binaryTree.Insert(10);
+            binaryTree.Insert(50);
+            binaryTree.Insert(30);
+            binaryTree.Insert(7);
+            binaryTree.Insert(100);
+            var result = binaryTree.SearchBinaryTree(7);
+            Assert.IsTrue(result.data == 7);
+        }
+    }
+}
